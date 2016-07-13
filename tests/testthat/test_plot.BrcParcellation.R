a0c06167e40a435a2d9c406099843d8ab542ebc9
Daniel Greenidge <cdgreenidge@users.noreply.github.com>
2016-07-13 12:16:38 -0400

    Remove arg checking in private functions

--- tests/testthat/test_plot.BrcParcellation.R
+++ tests/testthat/test_plot.BrcParcellation.R
@@ -2,10 +2,6 @@ context("Test plot.BrcParcellation.R")
 
 # Test .factorToNumeric()
 
-test_that("it should check that the first argument is a factor", {
-  expect_error(.factorToNumeric(numeric()), "argument must be a factor")
-})
-
 test_that("it should convert the factor to a numeric vector", {
   expect_equal(class(.factorToNumeric(factor(c(1, 2)))), "numeric")
 })
@@ -19,10 +15,6 @@ test_that("the numbers should map to the factor levels", {
 dim3d <- c(2, 2, 2)
 partition <- factor(c(0, 0, 1, 1, 2, 2, 3, 3))
 parcel <- brcbase::BrcParcellation(dim3d=dim3d, partition=partition)
-
-test_that("it should check that the first argument is a BrcParcellation", {
-  expect_error(.parcellationToArray(character()), "argument must be of class")
-})
 
 test_that("the dims of the array should be the dims of the parcellation", {
   arr <- .parcellationToArray(parcel)
