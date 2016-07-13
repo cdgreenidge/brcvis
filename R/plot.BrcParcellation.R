09e5ee8069f4089b21f72c1063cc49d7ea990101
Daniel Greenidge <cdgreenidge@users.noreply.github.com>
2016-07-13 11:55:14 -0400

    Add .parcellationToArray() function

--- R/plot.BrcParcellation.R
+++ R/plot.BrcParcellation.R
@@ -1,3 +1,11 @@
+.parcellationToArray <- function(parcellation) {
+  if (class(parcellation) != "BrcParcellation") {
+    stop("parcellation argument must be of class BrcParcellation")
+  }
+  data <- .factorToNumeric(parcellation$partition)
+  array(data=data, dim=parcellation$dim3d)
+}
+
 .factorToNumeric <- function(xs) {
   if (!is.factor(xs)) {
     stop("xs argument must be a factor")
--- tests/testthat/test_plot.BrcParcellation.R
+++ tests/testthat/test_plot.BrcParcellation.R
@@ -13,3 +13,24 @@ test_that("it should convert the factor to a numeric vector", {
 test_that("the numbers should map to the factor levels", {
   expect_equal(.factorToNumeric(factor(c(0, 2, 3))), c(0, 2, 3))
 })
+
+# Test .parcellationToArray()
+
+dim3d <- c(2, 2, 2)
+partition <- factor(c(0, 0, 1, 1, 2, 2, 3, 3))
+parcel <- brcbase::BrcParcellation(dim3d=dim3d, partition=partition)
+
+test_that("it should check that the first argument is a BrcParcellation", {
+  expect_error(.parcellationToArray(character()), "argument must be of class")
+})
+
+test_that("the dims of the array should be the dims of the parcellation", {
+  arr <- .parcellationToArray(parcel)
+  expect_equal(dim(arr), c(2, 2, 2))
+})
+
+test_that("the values in the array correspond to the partition", {
+  arr <- .parcellationToArray(parcel)
+  expected <- array(c(0, 0, 1, 1, 2, 2, 3, 3), dim=c(2, 2, 2))
+  expect_equal(arr, expected)
+})
