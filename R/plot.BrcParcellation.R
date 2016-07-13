3aa5db32e9d72ce56a4e1a0f8a2f825fa5eb2822
Daniel Greenidge <cdgreenidge@users.noreply.github.com>
2016-07-13 14:17:03 -0400

    Add .splitAlongDim function

--- R/plot.BrcParcellation.R
+++ R/plot.BrcParcellation.R
@@ -1,3 +1,11 @@
+# This function stolen from
+# http://stackoverflow.com/questions/20198751/three-dimensional-array-to-list
+.splitAlongDim <- function(arr, d) {
+  setNames(lapply(split(arr, arrayInd(seq_along(arr), dim(arr))[, d]),
+                  array, dim=dim(arr)[-d], dimnames(arr)[-d]),
+           dimnames(arr)[[d]])
+}
+
 .parcellationToArray <- function(parcellation) {
   data <- .factorToNumeric(parcellation$partition)
   array(data=data, dim=parcellation$dim3d)
