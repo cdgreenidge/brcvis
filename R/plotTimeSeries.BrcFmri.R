a7764f94d8f8943700853359bdbe953402a18109
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-08-08 17:14:02 -0400

    Added .convertVoxel3DGridto2D()

--- R/plotTimeSeries.BrcFmri.R
+++ R/plotTimeSeries.BrcFmri.R
@@ -1,6 +1,6 @@
 plotTimeSeries <- function(x, ...) UseMethod("plotTimeSeries")
 
-plotTimeSeries.BrcFmri <- function(x, xlim, ylim, zlim){
+plotTimeSeries.BrcFmri <- function(x, xlim, ylim, zlim, same.screen = TRUE){
   
 }
 
@@ -13,7 +13,14 @@ plotTimeSeries.BrcFmri <- function(x, xlim, ylim, zlim){
 }
 
 .convertVoxel3DGridto2D <- function(dim3d, xlim = NULL, ylim = NULL, zlim = NULL){
+  stopifnot(length(xlim) == 2, xlim[1] <= xlim[2])
+  stopifnot(length(ylim) == 2, ylim[1] <= ylim[2])
+  stopifnot(length(zlim) == 2, zlim[1] <= zlim[2])
   
+  xseq <- xlim[1]:xlim[2]; yseq <- ylim[1]:ylim[2]; zseq <- zlim[1]:zlim[2]
+  voxel.mat <- expand.grid(xseq, yseq, zseq)
+  
+  sort(apply(voxel.mat, 1, .convertVoxel3Dto2D, dim3d = dim3d))
 }
 
 .convertVoxel3Dto2D <- function(dim3d, vec){
--- tests/testthat/test_plotTimeSeries.BrcFmri.R
+++ tests/testthat/test_plotTimeSeries.BrcFmri.R
@@ -2,12 +2,21 @@ context("Test plotTimeSeries.BrcFmri.R")
 
 ## Test .convertVoxel3Dto2D()
 
-test_that("it returns the first value correctly",{
+test_that("it returns the first idx correctly",{
   dim3d = c(5,5,5)
   expect_true(.convertVoxel3Dto2D(dim3d, c(1,1,1)) == 1)
 })
 
-test_that("it returns the last value correctly",{
+test_that("it returns the last idx correctly",{
   dim3d = c(5,5,5)
   expect_true(.convertVoxel3Dto2D(dim3d, dim3d) == prod(dim3d))
+})
+
+######
+
+## Test .convertVoxel3DGridto2D()
+
+test_that("it returns the first idx correctly",{
+  dim3d = c(5,5,5)
+  expect_true(.convertVoxel3DGridto2D(dim3d, c(1,1), c(1,1), c(1,1)) == 1)
 })
